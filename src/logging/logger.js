15655ed5dde861d95c1bbc6e2d979b2d3fce9d51
Moyukh Bera <moyukh.bera@gmail.com>
2020-04-06 19:58:57 +0530

    added missing logging package

---
+++ src/logging/logger.js
@@ -0,0 +1,32 @@
+
+let debugLevel = 'warn';
+
+const log = (level, message) => {
+    var levels = ['info', 'warn', 'error'];
+    if (levels.indexOf(level) >= levels.indexOf(debugLevel)) {
+        if (typeof message !== 'string') {
+            message = JSON.stringify(message);
+        };
+        console.log(level + ': ' + message);
+    }
+}
+
+export const setLogLevel = (logLevel) => {
+    debugLevel = logLevel
+}
+
+export const debug = (msg) => {
+    log('debug', msg);
+}
+
+export const info = (msg) => {
+    log('info', msg);
+}
+
+export const warn = (msg) => {
+    log('warn', msg);
+}
+
+export const error = (msg) => {
+    log('error', msg);
+}
